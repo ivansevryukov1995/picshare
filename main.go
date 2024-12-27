18aed5651600f6c41a9aac3a89efff9e1a40528b
Ivan Sevryukov <sevryukovivan1995@mail.ru>
2024-12-27 20:10:34 +0300

    Add temp branch

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func galleriesHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	// userID := chi.URLParam(r, "userID")
+	fmt.Fprint(w, "<h1>Welcome !</h1>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -42,9 +49,15 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
 	r.Get("/", homeHandler)
+	r.Get("/galleries/", galleriesHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
